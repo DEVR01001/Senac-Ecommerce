093de3b712e41492f340006bcfd1535f8530fd17
DEVR01001 <rafael.dev01001@gmial.com>
2025-05-08 16:28:56 -0400

    visual cart

--- js/cart.js
+++ js/cart.js
@@ -13,3 +13,61 @@ function verifcarLogin(){
 
 
 verifcarLogin()
+
+
+
+const DivCart =  document.getElementById('cart_cont')
+
+
+
+
+
+
+
+
+
+
+let componenteCartProd = () =>{
+  
+    let cart = getCart()
+
+    console.log(cart)
+
+    cart.forEach(produto => {
+
+      
+      DivCart.innerHTML+= `
+
+        <tbody>
+            <tr class='flew-cart'>
+              <th scope="row"><img class='img-cart' hrfe='${produto.image}'> </img></th>
+              <td>${produto.nome}</td>
+              <td>${produto.price}</td>
+              <td>
+                  <button id='add_quant_cart'> <i class="bi bi-plus"></i></button>
+                  <span class='quantidade_cart' id='quant-cart'> </span>
+                   <button id='remove_quant_cart'> <i class="bi bi-dash"></i></button>
+              </td>
+              <td><i class="bi bi-x-lg"></i></td>
+            </tr>
+        </tbody>
+  
+  
+      `
+
+
+    });
+
+ 
+
+}
+
+
+
+componenteCartProd()
+
+
+function getCart() {
+  return JSON.parse(sessionStorage.getItem('cart') || '[]');
+}
+
--- js/solo-prod.js
+++ js/solo-prod.js
@@ -1,142 +1,95 @@
 
-
-function verifcarLogin(){
-
+function verificarLogin() {
     const nome = sessionStorage.getItem("nome");
     const email = sessionStorage.getItem("email");
-    const token = sessionStorage.getItem("id"); 
-  
-    if (!nome || !email || !token) {
+    const token = sessionStorage.getItem("id");
 
+    if (!nome || !email || !token) {
         alert("Você precisa estar logado para adicionar o produto no carrinho!");
         window.location.href = "login.html";
-  
-   
-      return false
+        return false;
     }
 
-    return true
-  
-  }
-
-  
-
-const DivSoloProd= document.getElementById('cont-prod-solo')
-
-const produtos = {}
-
-componenteSoloProd = (data) =>{
+    return true;
+}
 
 
+const DivSoloProd = document.getElementById('cont-prod-solo');
+
+
+const componenteSoloProd = (data) => {
     return `
-
         <div class="conatiner_header">
             <img src="${data.image}" alt="">
             <div class="container_text-prod">
-                <div class="cat_prod"> ${data.category} </div>
-                <div class="name_prod"> ${data.title} </div>
-                <div class="des_prod">  ${data.description} </div>
-                <div class="price_prod"> <span> Preço: </span> ${data.price} R$</div>
-                <div class='conatiner_btn_prod'> 
-
-                    <button href=""  id='add_cart' class="btn-add-cart">Add <i class="bi bi-cart"></i></button>
-                    <a href="" class="btn-favorite"><i class="bi bi-heart"></i></a>
-                
+                <div class="cat_prod">${data.category}</div>
+                <div class="name_prod">${data.title}</div>
+                <div class="des_prod">${data.description}</div>
+                <div class="price_prod"><span>Preço:</span> ${data.price} R$</div>
+                <div class="conatiner_btn_prod">
+                    <button id="add_cart" class="btn-add-cart">Add <i class="bi bi-cart"></i></button>
+                    <a href="#" class="btn-favorite"><i class="bi bi-heart"></i></a>
                 </div>
             </div>
         </div>
-
-    
-    `
+    `;
 }
-
-
-
-
-
-
 
 
 let idUrl = window.location.search;
+const parametros = new URLSearchParams(idUrl);
+const id = parametros.get("id");
 
-parametros = new URLSearchParams(idUrl)
-
-
-id = parametros.getAll(("id"))
-
-
-fetch(`https://fakestoreapi.com/products/${id[0]}`)
-.then(function(response){
-    return response.json()
-})
-.then( function(produto){
-    DivSoloProd.innerHTML = componenteSoloProd(produto)
-
-
-    const BtnAddCart =  document.getElementById('add_cart')
-    BtnAddCart.addEventListener('click', () =>{
-
-        if(!verifcarLogin()){
-        }else{
-
-            var prod = {
-
-                'id' : `${produto.id}`,
-                'image' : `${produto.image}`,
-                'nome' : `${produto.title}`,
-                'price' : `${produto.price}`,
-                'quantidade' : 1
-            }
-        
-
-            AddCart(prod)
-
-        }
-
+// Faz a requisição para pegar os dados do produto
+fetch(`https://fakestoreapi.com/products/${id}`)
+    .then(response => response.json())
+    .then(produto => {
     
-
-    })
-    
-})
-
-
-function AddCart (ObjectProd){
-
-    if(getCart().length <= 0){
-
-
-        let cart = getCart()
-
-
-        cart.push(ObjectProd)
-
-        sessionStorage.setItem('cart', JSON.stringify(cart))
-
-
-        
-    }else{
-
-        let catObject = getCart()
-      
+        DivSoloProd.innerHTML = componenteSoloProd(produto);
 
        
-        const produtoMudar = catObject.filter( item  => item.id === ObjectProd.id )
+        const BtnAddCart = document.getElementById('add_cart');
+        BtnAddCart.addEventListener('click', () => {
+            if (!verificarLogin()) {
+                return; 
+            } else {
+             
+                const prod = {
+                    id: `${produto.id}`,
+                    image: `${produto.image}`,
+                    nome: `${produto.title}`,
+                    price: `${produto.price}`,
+                    quantidade: 1
+                };
+                
+              
+                AddCart(prod);
+            }
+        });
+    });
 
-        // produtoMudar.quantidade = 1
 
-        console.log(catObject, produtoMudar.quantidade = 5)
+function AddCart(ObjectProd) {
+    let cart = getCart(); 
 
 
+    const produtoMudar = cart.filter(item => item.id === ObjectProd.id);
 
+    if (produtoMudar.length > 0) {
+        produtoMudar[0].quantidade += 1;
+        alert('Quantidade adicionada no carrinho')
+    } else {
+
+        cart.push(ObjectProd);
+        alert('Produto adicionado no carrinho')
     }
 
+    sessionStorage.setItem('cart', JSON.stringify(cart));
+    
 }
 
 
-
-function getCart(){
-
-    return JSON.parse(sessionStorage.getItem('cart') || '[]')
+function getCart() {
+    return JSON.parse(sessionStorage.getItem('cart') || '[]');
 }
-
 
